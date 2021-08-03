e711620e9d24aba96a50c6a07cd009a27367a9cd
Teddy <jfrancoisteddy@gmail.com>
2021-08-03 13:00:02 +0200

    feat : init window

---
+++ window.py
@@ -0,0 +1,22 @@
+import pygame
+
+pygame.init()
+
+display_width = 1000
+display_height = 800
+
+pygame.display.set_mode((display_width,display_height))
+pygame.display.set_caption("Shooter Br")
+
+icon = pygame.image.load('assets/icon.png')
+pygame.display.set_icon(icon)
+
+running = True
+
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            pygame.quit()
+            print("Window closed")
