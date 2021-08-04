6bdb0082fd2812eecaaea40a8830652968870268
Teddy <jfrancoisteddy@gmail.com>
2021-08-04 09:35:03 +0200

    feat : add Player to screen

--- window.py
+++ window.py
@@ -1,17 +1,25 @@
 import pygame
+import sys
+from classes import Player
 
 pygame.init()
 
+# Game screen
 display_width = 1000
 display_height = 800
 
-pygame.display.set_mode((display_width,display_height))
+screen = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption("Shooter Br")
 
 icon = pygame.image.load('assets/icon.png')
 pygame.display.set_icon(icon)
 
 running = True
+
+# A container class to hold and manage multiple Sprite objects.
+moving_sprites = pygame.sprite.Group()
+player = Player.Player(100, 100)
+moving_sprites.add(player)
 
 while running:
 
@@ -20,3 +28,11 @@ while running:
             running = False
             pygame.quit()
             print("Window closed")
+            sys.exit()
+        if event.type == pygame.K_RIGHT:
+            player.walk_right()
+
+    screen.fill((255, 255, 255))
+    moving_sprites.draw(screen)
+    moving_sprites.update(0.25)
+    pygame.display.flip()
